4849d2cc09cb8312d3d8f550b75cc75269c248bb
Laszlo Havasi <b05xf0@inf.elte.hu>
2024-01-01 20:38:36 +0100

    add day 1 - part 2

--- day_01.hs
+++ day_01.hs
@@ -1,4 +1,8 @@
-import Data.Char
+import Data.Char ( isDigit )
+import Data.List ( isPrefixOf )
+
+fixed :: Bool
+fixed = True
 
 main :: IO ()
 main = do
@@ -6,10 +10,32 @@ main = do
   print $ sumOfCalibrationValues doc
 
 sumOfCalibrationValues :: String -> Int
-sumOfCalibrationValues calibrationDoc = sum $ read . getFirstAndLast <$> getNumbers 
+sumOfCalibrationValues calibrationDoc = sum $ (calibrationValue []) <$> lines calibrationDoc 
+
+calibrationValue :: String -> String -> Int
+calibrationValue []           []                   = 0
+calibrationValue [x]          []                   = read [x, x]
+calibrationValue digits       []                   = read digits
+calibrationValue []           line@(x:xs)
+  | isDigit x                                      = calibrationValue [x] xs
+  | fixed                                          = calibrationValue (digitSpelledOutWithLetters line) xs
+  | otherwise                                      = calibrationValue [] xs
+calibrationValue digits@(y:_) line@(x:xs)
+  | isDigit x                                      = calibrationValue [y, x] xs
+  | fixed && digitSpelledOutWithLetters line /= [] = calibrationValue (y:digitSpelledOutWithLetters line) xs
+  | otherwise                                      = calibrationValue digits xs
+
+digitSpelledOutWithLetters :: String -> String
+digitSpelledOutWithLetters line = take 1 [v | (k, v) <- dict, k `isPrefixOf` line]
   where
-    getNumbers = filter isDigit <$> lines calibrationDoc
-    getFirstAndLast numbers = case numbers of
-      []     -> "0"
-      (x:[]) -> [x, x]
-      (x:xs) -> [x, last xs]
+    dict =
+      [("one"  , '1')
+      ,("two"  , '2')
+      ,("three", '3')
+      ,("four" , '4')
+      ,("five" , '5')
+      ,("six"  , '6')
+      ,("seven", '7')
+      ,("eight", '8')
+      ,("nine" , '9')
+      ,("zero" , '0')]
