3d4e7ee584ecd80fd9ba1f144257079756cb24a0
Laszlo Havasi <b05xf0@inf.elte.hu>
2024-01-03 23:41:20 +0100

    cleanup

--- day_02.hs
+++ day_02.hs
@@ -6,43 +6,39 @@ main = do
   let games = parseGame <$> lines doc
   print $ sum (fst <$> filter possible games)
 
-stockConfig :: [Cube]
-stockConfig = 
-  [("red"  , 12)
-  ,("green", 13)
-  ,("blue" , 14)]
-
-games :: String -> [Game]
-games doc = parseGame <$> lines doc
-
-possible :: Game -> Bool
-possible game = all (\(color, count) -> count <= stock color) sets
-  where sets = concat $ snd game
-
-stock :: String -> Int
-stock color = case lookup color stockConfig of
-  Nothing    -> 0
-  Just count -> count
-
 type Game = (Int, [Set])
 
 type Set = [Cube]
 
 type Cube = (String, Int)
 
+config :: [Cube]
+config = 
+  [("red"  , 12)
+  ,("green", 13)
+  ,("blue" , 14)]
+
+possible :: Game -> Bool
+possible game = all (\(color, count) -> count <= stock color config) sets
+  where sets = concat $ snd game
+
+stock :: String -> [Cube] -> Int
+stock color set = case lookup color set of
+  Nothing    -> 0
+  Just count -> count
+
 parseGame :: String -> Game
-parseGame src = (gameId, sets)
+parseGame src = (read gameIdSrc, parseSet <$> splitOn ';' setsSrc)
   where
     (heading, (':':setsSrc)) = span (/=':') src
-    [_, gameId] = read <$> words heading
-    sets = parseSet <$> splitOn ';' setsSrc
+    [_, gameIdSrc] = words heading
 
 parseSet :: String -> [Cube]
 parseSet src = parseCube <$> splitOn ',' src
 
 parseCube :: String -> Cube
-parseCube src = (cubeColor, read count)
-  where [count, cubeColor] = words src
+parseCube src = (color, read countSrc)
+  where [countSrc, color] = words src
       
 splitOn :: Char -> String -> [String]
 splitOn delimiter s = case dropWhile (== delimiter) s of
