524ed4336e25d58e6539e471983f9304006fcaa9
Laszlo Havasi <b05xf0@inf.elte.hu>
2024-01-03 23:26:56 +0100

    add day 2 - part 1

--- day_02.hs
+++ day_02.hs
@@ -1,16 +1,51 @@
 import Data.Char ( toUpper )
 
-data Cube = Green Int | Red Int | Blue Int deriving (Show, Read, Eq)
+main :: IO ()
+main = do
+  doc <- readFile "day_02_input.txt"
+  let games = parseGame <$> lines doc
+  print $ sum (fst <$> filter possible games)
+
+stockConfig :: [Cube]
+stockConfig = 
+  [("red"  , 12)
+  ,("green", 13)
+  ,("blue" , 14)]
+
+games :: String -> [Game]
+games doc = parseGame <$> lines doc
+
+possible :: Game -> Bool
+possible game = all (\(color, count) -> count <= stock color) sets
+  where sets = concat $ snd game
+
+stock :: String -> Int
+stock color = case lookup color stockConfig of
+  Nothing    -> 0
+  Just count -> count
+
+type Game = (Int, [Set])
 
 type Set = [Cube]
 
-data Game = Game Int [Set] deriving (Show, Read, Eq)
+type Cube = (String, Int)
 
-parseGame = span (/=':')
+parseGame :: String -> Game
+parseGame src = (gameId, sets)
+  where
+    (heading, (':':setsSrc)) = span (/=':') src
+    [_, gameId] = read <$> words heading
+    sets = parseSet <$> splitOn ';' setsSrc
+
+parseSet :: String -> [Cube]
+parseSet src = parseCube <$> splitOn ',' src
 
 parseCube :: String -> Cube
-parseCube src = read $ unwords [toUpper colorHead:colorTail, count]
-  where
-    [count, colorHead:colorTail] = words src
-
-
+parseCube src = (cubeColor, read count)
+  where [count, cubeColor] = words src
+      
+splitOn :: Char -> String -> [String]
+splitOn delimiter s = case dropWhile (== delimiter) s of
+  "" -> []
+  s' -> w : splitOn delimiter s''
+    where (w, s'') = break (== delimiter) s'
