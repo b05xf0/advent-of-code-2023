fc32bc14b4183ecd756e105250780d33196c0bca
Laszlo Havasi <b05xf0@inf.elte.hu>
2024-01-04 21:27:47 +0100

    add day 2 - part 2

--- day_02.hs
+++ day_02.hs
@@ -1,8 +1,11 @@
+import Data.List ( sortOn )
+
 main :: IO ()
 main = do
   doc <- readFile "day_02_input.txt"
   let games = parseGame <$> lines doc
   print $ sum (fst <$> filter possible games)
+  print $ sum (powerOfSetNeeded <$> games)
 
 type Game = (Int, [Set])
 
@@ -10,15 +13,21 @@ type Set = [Cube]
 
 type Cube = (String, Int)
 
-config :: [Cube]
+config :: Set
 config = 
   [("red"  , 12)
   ,("green", 13)
   ,("blue" , 14)]
 
 possible :: Game -> Bool
-possible game = all (\(color, count) -> count <= stock color config) sets
-  where sets = concat $ snd game
+possible game = all (\(color, count) -> count <= stock color config) $ allSets game
+
+powerOfSetNeeded :: Game -> Int
+powerOfSetNeeded game = product $ (\(color, _) -> stock color orderedSets) <$> config
+  where orderedSets = reverse . (sortOn snd) $ allSets game 
+
+allSets :: Game -> [Cube]
+allSets game = concat $ snd game
 
 stock :: String -> [Cube] -> Int
 stock color set = case lookup color set of
@@ -30,7 +39,7 @@ parseGame src = (read gameIdSrc, parseSet <$> splitOn ';' setsSrc)
   where
     (_:_:_:_:_:gameIdSrc, _:setsSrc) = span (/=':') src
 
-parseSet :: String -> [Cube]
+parseSet :: String -> Set
 parseSet src = parseCube <$> splitOn ',' src
 
 parseCube :: String -> Cube
