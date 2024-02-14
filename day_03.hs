4458031ec7d133530762c661861f61ac7f9f985c
Laszlo Havasi <b05xf0@inf.elte.hu>
2024-02-15 00:27:23 +0100

    prep day 3

--- day_03.hs
+++ day_03.hs
@@ -1,10 +1,31 @@
+import Data.Char ( isDigit )
+
+type Engine = [(Part, Position)]
+
+data Part = Symbol Char | Number [Char]
+  deriving (Show)
+
+type Position = (Int, Int)
+
 main :: IO ()
 main = do
-  engine <- readFile "day_03_input.txt"
-  let (width, height) = dim engine
-  print (width, height)
+  engine <- parse <$> readFile "day_03_input.txt"
+  print $ sumOfPartNumbers engine
 
-dim :: String -> (Int, Int)
-dim engine = case lines engine of
-  []       -> (0,0)
-  ls@(l:_) -> (length l, length ls)
+parse :: String -> Engine
+parse raw = parse' raw (0, 0) []
+  where
+    parse' []         _          processed = processed
+    parse' raw@(c:cs) pos@(x, y) processed
+      | c == '.'                           = parse' cs (x + 1, y) processed
+      | c == '\n'                          = parse' cs (0, y + 1) processed
+      | isDigit c                          = let (num, rest) = span isDigit raw
+                                             in parse' rest (x + length num, y) $ (Number num, pos):processed
+      | otherwise                          = parse' cs (x + 1, y) $ (Symbol c, pos):processed
+
+sumOfPartNumbers :: Engine -> Int
+sumOfPartNumbers engine = sum [valueOf part | part <- engine]
+  where
+    valueOf (Number num, pos) = if isPartNumber pos then read num else 0
+    valueOf _                 = 0 
+    isPartNumber pos = True
