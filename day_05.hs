2d5874b7a115077bd01acd317de238596e40ec98
Laszlo Havasi <b05xf0@inf.elte.hu>
2024-02-26 22:50:15 +0100

    add d5

--- day_05.hs
+++ day_05.hs
@@ -3,8 +3,11 @@ module Main where
 main :: IO ()
 main = do
   (seeds, mappings) <- parse <$> readFile "day_05_input.txt"
-  print seeds
-  print mappings
+  let locations     = convert mappings <$> seeds
+      moreSeeds     = concat $ rangesToSeeds seeds
+      moreLocations = convert mappings <$> moreSeeds
+  putStrLn $ "d05p1: " <> show (minimum $ (\(Location id) -> id) <$> locations)
+  putStrLn $ "d05p2: " <> show (minimum $ (\(Location id) -> id) <$> moreLocations)
 
 data Category = Seed Int
               | Soil Int
@@ -16,19 +19,37 @@ data Category = Seed Int
               | Location Int
   deriving ( Show )
 
-type Mapping = (String, [(Int, Int, Int)])
+type Mapping = (String, [[Int]])
 
 parse :: String -> ([Category], [Mapping])
-parse raw = parse' (words <$> lines raw) ([], []) 
-  where 
+parse raw = parse' (words <$> lines raw) ([], [])
+  where
     parse' src parsed = case (src, parsed) of
       ([]:rest, parsed)                        -> parse' rest parsed
       (("seeds:":ss):rest, (seeds, mappings))  -> parse' rest (Seed . read <$> ss, mappings)
-      ((key:"map:":_):rest, (seeds, mappings)) -> parse' (dropWhile (/= []) rest) 
-                                                         (seeds, (key, parseMapping (takeWhile (/= []) rest)) : mappings)
+      ((key:"map:":_):rest, (seeds, mappings)) -> parse' (dropWhile (/= []) rest)
+                                                         (seeds, (key, map (read <$>) (takeWhile (/= []) rest)) : mappings)
       (_, parsed)                              -> parsed
 
-parseMapping :: [[String]] -> [(Int, Int, Int)]
-parseMapping src = case src of
-  [destFrom, srcFrom, len]:rest -> (read destFrom, read srcFrom, read len) : parseMapping rest
-  _                             -> []
+convert :: [Mapping] -> Category -> Category
+convert mappings sth = case sth of
+  Seed id        -> convert mappings (Soil (id `searchIn` "seed-to-soil"))
+  Soil id        -> convert mappings (Fertilizer (id `searchIn` "soil-to-fertilizer"))
+  Fertilizer id  -> convert mappings (Water (id `searchIn` "fertilizer-to-water"))
+  Water id       -> convert mappings (Light (id `searchIn` "water-to-light"))
+  Light id       -> convert mappings (Temperature (id `searchIn` "light-to-temperature"))
+  Temperature id -> convert mappings (Humidity (id `searchIn` "temperature-to-humidity"))
+  Humidity id    -> Location (id `searchIn` "humidity-to-location") 
+  where
+    searchIn id mappingKey = case [destFrom + id - srcFrom |
+                                     (key, mapping) <- mappings,
+                                     key == mappingKey,
+                                     [destFrom, srcFrom, len] <- mapping,
+                                     id >= srcFrom && id < srcFrom + len] of
+      [newId] -> newId
+      _       -> id
+
+rangesToSeeds :: [Category] -> [[Category]]
+rangesToSeeds ranges = case ranges of
+  Seed from:Seed len:rest -> (Seed <$> [from .. from + len - 1]) : rangesToSeeds rest
+  _                       -> []
